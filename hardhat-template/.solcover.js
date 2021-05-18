fa6fff722adc34acce6221fb334ee13ef8d6d2d0
Veronica <veronica.21.coutts@gmail.com>
2021-05-18 12:29:59 +0200

    Hardhat template

---
+++ hardhat-template/.solcover.js
@@ -0,0 +1,7 @@
+module.exports = {
+    /**
+     * Skip file path starts in contract folder. Does not support special chars
+     * i.e *.
+     */
+    // skipFiles: ['Routers/EtherRouter.sol']
+};
